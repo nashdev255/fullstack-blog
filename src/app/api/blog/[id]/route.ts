ee5c1c2835dfcae8737f7bbe659fc3c33a7f71f5
nashdev255 <yutori1107@gmail.com>
2023-09-04 18:37:24 +0900

    add put-api

--- src/app/api/blog/[id]/route.ts
+++ src/app/api/blog/[id]/route.ts
@@ -10,7 +10,27 @@ export const GET = async (req: Request, res: NextResponse) => {
         const id: number = parseInt(req.url.split("/blog/")[1]);
         await main();
         const post = await prisma.post.findFirst({ where: { id } });
-        return NextResponse.json({ message: "success", post }, { status: 200 });
+        return NextResponse.json({ message: "Success", post }, { status: 200 });
+    } catch(err) {
+        return NextResponse.json({ message: "Error", err }, { status: 500 });
+    } finally {
+        await prisma.$disconnect();
+    }
+};
+
+// ブログ記事編集用API
+export const PUT = async (req: Request, res: NextResponse) => {
+    try {
+        const id: number = parseInt(req.url.split("/blog/")[1]);
+
+        const { title, description } = await req.json();
+
+        await main();
+        const post = await prisma.post.update({
+            data: { title, description },
+            where: { id },
+        });
+        return NextResponse.json({ message: "Success", post }, { status: 200 });
     } catch(err) {
         return NextResponse.json({ message: "Error", err }, { status: 500 });
     } finally {
--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -8,7 +8,7 @@ export async function main() {
     try {
         await prisma.$connect();
     } catch(err) {
-        return Error("failed to connect database");
+        return Error("Failed to connect database");
     }
 }
 
@@ -17,7 +17,7 @@ export const GET = async (req: Request, res: NextResponse) => {
     try {
         await main();
         const posts = await prisma.post.findMany();
-        return NextResponse.json({ message: "success", posts }, { status: 200 });
+        return NextResponse.json({ message: "Success", posts }, { status: 200 });
     } catch(err) {
         return NextResponse.json({ message: "Error", err }, { status: 500 });
     } finally {
