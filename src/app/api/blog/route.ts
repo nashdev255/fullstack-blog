833c4d3c02a24f07a529ba6f467eb5d2bd358788
nashdev255 <yutori1107@gmail.com>
2023-09-04 15:56:18 +0900

    get-api, post-api

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -1,5 +1,40 @@
+import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
+const prisma = new PrismaClient();
+
+// DB接続
+export async function main() {
+    try {
+        await prisma.$connect();
+    } catch(err) {
+        return Error("failed to connect database");
+    }
+}
+
+// ブログ全記事取得用API
 export const GET = async (req: Request, res: NextResponse) => {
-    console.log("GET");
+    try {
+        await main();
+        const posts = await prisma.post.findMany();
+        return NextResponse.json({ message: "success", posts }, { status: 200 });
+    } catch(err) {
+        return NextResponse.json({ message: "Error", err }, { status: 500 });
+    } finally {
+        await prisma.$disconnect();
+    }
+};
+
+// ブログ記事投稿用API
+export const POST = async (req: Request, res: NextResponse) => {
+    try {
+        const { title, description } = await req.json();
+        await main();
+        const post = await prisma.post.create({ data: { title, description }});
+        return NextResponse.json({ message: "success", post }, { status: 201 });
+    } catch(err) {
+        return NextResponse.json({ message: "Error", err }, { status: 500 });
+    } finally {
+        await prisma.$disconnect();
+    }
 };
